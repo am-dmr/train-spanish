0b31130b0ba2588a07c50163a6021b3842d6fc62
Mikhail Alekseev <mikhail.alexeev.dmr@gmail.com>
2020-10-01 16:08:26 +0200

    Small fixes

---
+++ db/migrate/20201001140206_add_devise_to_users.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class AddDeviseToUsers < ActiveRecord::Migration[6.0]
+  def self.up
+    change_table :users do |t|
+      ## Database authenticatable
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Rememberable
+      t.datetime :remember_created_at
+    end
+
+    add_index :users, :email, unique: true
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
