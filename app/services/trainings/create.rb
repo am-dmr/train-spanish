806bae205a48ce13fcfeb775c14e02a67c117b81
Mikhail Alekseev <mikhail.alexeev.dmr@gmail.com>
2020-10-13 18:06:07 +0200

    Tenses refactoring

--- app/services/trainings/create.rb
+++ app/services/trainings/create.rb
@@ -15,54 +15,80 @@ class Trainings::Create < BaseService
 
   private
 
+  def additional_input
+    @additional_input ||= {}
+  end
+
+  def result
+    @result ||= 0
+  end
+
+  def user_input
+    @user_input ||= ''
+  end
+
+  def tenses_coeff
+    @tenses_coeff ||= 1
+  end
+
   def es2ru
-    result = word.russian.casecmp?(russian) ? 100 : 0
-    result_tenses = calc_tenses_result
-    result *= result_tenses[1]
-    user_input = "#{russian}\n#{result_tenses[0]}"
-    Training.create(user: user, word: word, user_input: user_input, result: result)
+    @result = word.russian.casecmp?(russian) ? 100 : 0
+    @user_input = russian
+    calc_tenses_result
+    create_record
   end
 
   def ru2es
-    result = 0
-    result += 10 if article.blank? || word.articles.any? { |a| a.casecmp?(article) }
-    result += 90 if I18n.transliterate(word.spanish).casecmp?(I18n.transliterate(spanish))
-    result_tenses = calc_tenses_result
-    result *= result_tenses[1]
-    user_input = "#{article} #{spanish}\n#{result_tenses[0]}"
-    Training.create(user: user, word: word, user_input: user_input, result: result)
+    @result = 0
+    @result += 10 if article.blank? || word.articles.any? { |a| a.casecmp?(article) }
+    @result += 90 if spanish_casecmp?(word.spanish, spanish)
+    @user_input = "#{article} #{spanish}"
+
+    calc_tenses_result
+    create_record
+  end
+
+  def spanish_casecmp?(first, second)
+    I18n.transliterate(first).casecmp?(I18n.transliterate(second))
   end
 
   def calc_tenses_result
-    return nil, 1 unless word.part_of_speech_verb?
+    return unless word.part_of_speech_verb?
 
-    buf = []
+    result_input = {}
     total = 0
     correct = 0
 
     VerbForm.tenses.each_key do |tense|
-      buf_per_tense = []
+      result_input[tense] = []
       VerbForm.pronouns.each_key do |pronoun|
-        verb_form = verb_forms.find { |vf| vf.tense == tense && vf.pronoun == pronoun }
-        next if verb_form.blank?
-
-        input = tenses.dig(tense, pronoun)
-        next if input.blank?
+        verb_form = find_verb_form(tense, pronoun)
+        user_input = tenses.dig(tense, pronoun)
+        result_input[tense] << user_input
+        next if verb_form.blank? || user_input.blank?
 
         total += 1
-        buf_per_tense << input
-        correct += 1 if I18n.transliterate(verb_form.spanish).casecmp?(input)
+        correct += 1 if spanish_casecmp?(verb_form.spanish, user_input)
       end
-      buf << buf_per_tense.join(' - ')
     end
 
-    return '', 1 if total.zero?
+    @tenses_coeff = 0.5 + 0.5 * correct.to_f / total
+    @additional_input[:tenses] = result_input
+  end
 
-    coeff = 0.5 + 0.5 * correct.to_f / total
-    [buf.join("\n"), coeff]
+  def create_record
+    Training.create(user: user,
+                    word: word,
+                    user_input: user_input,
+                    additional_input: additional_input,
+                    result: result * tenses_coeff)
   end
 
   def verb_forms
     @verb_forms ||= word.verb_forms
+  end
+
+  def find_verb_form(tense, pronoun)
+    verb_forms.find { |vf| vf.tense == tense && vf.pronoun == pronoun }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_133845) do
+ActiveRecord::Schema.define(version: 2020_10_13_152616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_10_12_133845) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "user_input", default: "", null: false
+    t.jsonb "additional_input"
     t.index ["user_id"], name: "index_trainings_on_user_id"
     t.index ["word_id"], name: "index_trainings_on_word_id"
   end
